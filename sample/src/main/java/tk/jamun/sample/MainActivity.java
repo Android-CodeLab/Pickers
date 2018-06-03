35e06303ceaa9412db6b40fb0114cd02c2908a1a
LibJamun <jatinsahgal50@gmail.com>
2018-06-04 00:35:19 +0530

    0.0.1(v3) sample with app logo and Element Two Libs

--- sample/src/main/java/tk/jamun/sample/MainActivity.java
+++ sample/src/main/java/tk/jamun/sample/MainActivity.java
@@ -5,13 +5,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import tk.jamun.elements.circularimageview.CircularImageView;
+import tk.jamun.elements.circularnetworkimageview.CircularNetworkImageView;
+import tk.jamun.volley.classes.VolleyImageDownload;
+import tk.jamun.volley.main.VolleySingleton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initializeView();
+    }
+    private void initializeView() {
+        CircularImageView circularImageView = findViewById(R.id.id_circle_image);
+        CircularNetworkImageView circularNetworkImageView = findViewById(R.id.id_circle_network_image);
+        VolleySingleton.setInstance(this);
+        VolleyImageDownload.getInstance().downloadImage("https://techcruzers.com/images/team/members/jatin-sahgal.jpg", circularNetworkImageView,
+                R.drawable.image_vd_user_default);
+        VolleyImageDownload.getInstance().downloadImage("https://techcruzers.com/images/team/members/jatin-sahgal.jpg", circularImageView,
+                R.drawable.image_vd_user_default);
     }
 
     public void onClickPicker(View view) {
